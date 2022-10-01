fc00b8e16b74763ce510837239eec1a2f24b1043
davi38 <davi4alves@gmail.com>
2022-10-01 15:32:32 -0300

    fix: enviroment variables not accepted

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 //@desc Register new user
 //@route POST /api/users
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const colors = require('colors')
-const {errorHandler} = require('./middleware/errorMiddleware')
+require("dotenv").config();
+const express = require("express");
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 const connectDB = require("./config/db.js");
+const { populate } = require("./models/userModel");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals',require('./routes/goalRoutes'))
-app.use('/api/users',require('./routes/userRoutes'))
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.use(errorHandler)  //change default error
+app.use(errorHandler); //change default error
 
 const port = process.env.PORT || 3001;
-connectDB()
-app.listen(port, () => console.log(`Server running on port ${port}`))
+connectDB();
+/* console.log(process.env); */
+app.listen(port, () => console.log(`Server running on port ${port}`));
